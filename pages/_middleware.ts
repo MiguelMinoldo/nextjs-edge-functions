8e8009edafbd439da6068407d726038df301486c
miguel.minoldo <miguel.minoldo@valtech.com>
2022-06-16 14:36:35 +0200

    request data logging

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,14 +2,13 @@ import { NextResponse } from 'next/server'
 import type { NextFetchEvent, NextRequest  } from 'next/server'
 
 export function middleware(req: NextRequest, event: NextFetchEvent ) {
-    // Store the response so we can modify its headers
-    const response = NextResponse.next()
-
-    console.log('User coming from: ' + req.geo?.country);
-
-    if (req.geo?.country === 'FR') {
-        response.headers.set('x-country-fr', 'true')
+    const reqData = {
+        url: req.url,
+        ip: req.ip,
+        ua: req.ua,
+        geo: req.geo
     }
+       console.log(reqData);
 
-  return response;
+    return new Response(JSON.stringify(reqData));
 }
